b165871b89b873a78764fa687977488ca117b9d7
coco <angel.suarez@estudiantesunibague.edu.co>
2026-08-23 23:51:41 -0500

    Se agrego PacienteRepositoryImpl

---
+++ model/PacienteRepositoryImpl.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PacienteRepositoryImpl implements IPacienteRepository {
+
+    private List<Paciente> listaPacientes;
+
+    public PacienteRepositoryImpl() {
+        this.listaPacientes = new ArrayList<>();
+    }
+
+    @Override
+    public Paciente buscarPorId(String id) {
+        for (Paciente p : listaPacientes) {
+            if (p.getIdPaciente().equals(id)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean guardarPaciente(Paciente p) {
+        Paciente existente = buscarPorId(p.getIdPaciente());
+        if (existente != null) {
+            listaPacientes.remove(existente);
+        }
+        listaPacientes.add(p);
+        return true;
+    }
+
+    @Override
+    public List<Paciente> obtenerTodos() {
+        return listaPacientes;
+    }
+
+    @Override
+    public boolean eliminar(String idPaciente) {
+        Paciente paciente = buscarPorId(idPaciente);
+        if (paciente == null) return false;
+        return listaPacientes.remove(paciente);
+    }
+}
